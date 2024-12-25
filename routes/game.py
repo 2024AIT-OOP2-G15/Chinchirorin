8b75fd32586c309cf1ab87b80d287f4461ff7d9b
ryota <mc_ryouta3@yahoo.co.jp>
2024-12-25 09:46:19 +0900

    [update] 仮でサイコロの処理を追加

--- routes/game.py
+++ routes/game.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 # Blueprintの作成
 game_bp = Blueprint('game', __name__, url_prefix='/game')
 
-@game_bp.route('/')
+@game_bp.route('/', methods=['POST'])
 def index():
-    return render_template('game.html')
+    name = request.form['name']
+    return render_template('game.html',name=name)
