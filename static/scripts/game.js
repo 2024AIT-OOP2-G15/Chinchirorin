93f988bdaed9f783f06f9b62d0d996372eb024d7
ryota <mc_ryouta3@yahoo.co.jp>
2025-01-01 15:18:52 +0900

    [add] 役表（仮）を追加

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -4,7 +4,13 @@ function roll() {
   // 3つの乱数を取得（１〜６）
   const rand1 = Math.floor(Math.random() * 6) + 1;
   const rand2 = Math.floor(Math.random() * 6) + 1;
-  const rand3 = Math.floor(Math.random() * 6) + 1;
+  let rand3 = Math.floor(Math.random() * 6) + 1; // ションベン判定のためこれだけ可変
+
+  // ションベン判定
+  const diceOut = Math.floor(Math.random() * 50); // ダイスが飛び出したかの判定
+  if(diceOut == 0) {
+    rand3 = 0 // 0が飛び出した判定
+  }
 
   // HTMLに表示
   const disp = document.getElementById("number")
