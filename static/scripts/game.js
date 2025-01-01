db1cbb1bee6c38251f6d03877f0b902cd91fe5b6
ryota <mc_ryouta3@yahoo.co.jp>
2025-01-01 15:56:47 +0900

    [update] 役判定を仮追加

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -2,17 +2,46 @@
 
 function roll() {
   // 3つの乱数を取得（１〜６）
-  const rand1 = Math.floor(Math.random() * 6) + 1;
-  const rand2 = Math.floor(Math.random() * 6) + 1;
-  let rand3 = Math.floor(Math.random() * 6) + 1; // ションベン判定のためこれだけ可変
+  let rand1 = Math.floor(Math.random() * 6) + 1;
+  let rand2 = Math.floor(Math.random() * 6) + 1;
+  let rand3 = Math.floor(Math.random() * 6) + 1;
 
   // ションベン判定
-  const diceOut = Math.floor(Math.random() * 50); // ダイスが飛び出したかの判定
-  if(diceOut == 0) {
-    rand3 = 0 // 0が飛び出した判定
+  if (Math.floor(Math.random() * 50) === 0) {
+    rand1 = 0; // 0が飛び出した判定
+  }
+  if (Math.floor(Math.random() * 50) === 0) {
+    rand2 = 0; // 0が飛び出した判定
+  }
+  if (Math.floor(Math.random() * 50) === 0) {
+    rand3 = 0; // 0が飛び出した判定
   }
 
+  const yaku = hantei(rand1, rand2, rand3)
+
   // HTMLに表示
-  const disp = document.getElementById("number")
-  disp.textContent = `${rand1} ${rand2} ${rand3}`
+  const diceDisp = document.getElementById("number");
+  diceDisp.textContent = `${rand1} ${rand2} ${rand3}`;
+
+  const hanteiDisp = document.getElementById('hantei');
+  hanteiDisp.textContent = yaku;
+}
+
+function hantei(num1, num2, num3) {
+
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "ションベン";
+  } else if (num1 === 1 && num2 === 1 && num3 === 1) {
+    return "ピンゾロ";
+  } else if (num1 === num2 && num1 === num3 && num2 === num3) {
+    return "ゾロ目";
+  } else if ([num1, num2, num3].sort((a, b) => a - b).toString() === [4, 5, 6].toString()){
+    return "シゴロ";
+  } else if (num1 === num2 || num1 === num3 || num2 === num3) {
+    return "通常の目"
+  } else if ([num1, num2, num3].sort((a, b) => a - b).toString() === [1, 2, 3].toString()) {
+    return "ヒフミ";
+  }
+
+  return "役なし";
 }
