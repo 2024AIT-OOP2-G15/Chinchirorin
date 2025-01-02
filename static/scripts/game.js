64b4f4586fcfd0c4f265e1ffe376840fd4dafa88
ryota <mc_ryouta3@yahoo.co.jp>
2025-01-02 13:00:33 +0900

    [update] ターン切り替え（表示部分だけ）実装

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -1,5 +1,27 @@
 // 仮で数字を送るだけの処理を書いています。
 
+// グローバル変数
+let turn = "あなた";
+
+// 初期化
+document.addEventListener('DOMContentLoaded', (event) => {
+  const turnDisp = document.getElementById("turn");
+  turnDisp.textContent = turn;
+});
+
+// ターン切り替え
+function toggleTurn() {
+  if (turn === "あなた") {
+    turn = "CPU";
+  } else if (turn === "CPU") {
+    turn = "あなた";
+  }
+
+  const turnDisp = document.getElementById("turn");
+  turnDisp.textContent = turn;
+}
+
+// サイコロの処理
 function roll() {
   // 3つの乱数を取得（１〜６）
   let rand1 = Math.floor(Math.random() * 6) + 1;
@@ -17,29 +39,37 @@ function roll() {
     rand3 = 0; // 0が飛び出した判定
   }
 
-  const yaku = hantei(rand1, rand2, rand3)
+  // 役判定
+  const yaku = hantei(rand1, rand2, rand3);
+
+  // ターン切り替え
+  toggleTurn();
 
   // HTMLに表示
   const diceDisp = document.getElementById("number");
   diceDisp.textContent = `${rand1} ${rand2} ${rand3}`;
 
-  const hanteiDisp = document.getElementById('hantei');
+  const hanteiDisp = document.getElementById("hantei");
   hanteiDisp.textContent = yaku;
 }
 
+// 役判定
 function hantei(num1, num2, num3) {
-
   if (num1 === 0 || num2 === 0 || num3 === 0) {
     return "ションベン";
   } else if (num1 === 1 && num2 === 1 && num3 === 1) {
     return "ピンゾロ";
   } else if (num1 === num2 && num1 === num3 && num2 === num3) {
     return "ゾロ目";
-  } else if ([num1, num2, num3].sort((a, b) => a - b).toString() === [4, 5, 6].toString()){
+  } else if (
+    [num1, num2, num3].sort((a, b) => a - b).toString() === [4, 5, 6].toString()
+  ) {
     return "シゴロ";
   } else if (num1 === num2 || num1 === num3 || num2 === num3) {
-    return "通常の目"
-  } else if ([num1, num2, num3].sort((a, b) => a - b).toString() === [1, 2, 3].toString()) {
+    return "通常の目";
+  } else if (
+    [num1, num2, num3].sort((a, b) => a - b).toString() === [1, 2, 3].toString()
+  ) {
     return "ヒフミ";
   }
 
